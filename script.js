8d88b8d9f3068cc5a4c8ce2379cd07a4fcd0509b
Sebastian <benay199904@gmail.com>
2022-09-05 22:08:34 -0300

    Subiendo maqueta basica de html , con botones agregar palabra y cancelar con funciones

---
+++ script.js
@@ -0,0 +1,8 @@
+let btnIniciar = document.querySelector("#btn-iniciar");
+let btnAgregar = document.querySelector("#btn-agregar-palabra");
+let btnCancelar = document.querySelector("#btn-cancelar");
+
+
+btnAgregar.addEventListener("click", () => {
+    location.href = "agregarPalabra.html";
+})
